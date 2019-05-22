408b5ab6286a48178a505d22e2e1fe0254ccf395
Danil Semelenov <mail@danil.mobi>
2019-05-23 00:42:24 +0300

    Add a fallback for a lack of IntersectionObserver

--- scripts/cover-lazy-loading.js
+++ scripts/cover-lazy-loading.js
@@ -1,17 +1,21 @@
+const addCoverClass = cover =>
+  cover.classList.add(`cover-${cover.dataset.cover}`);
+
 export default () => {
+  const covers = [...document.querySelectorAll('.cover')];
   if (window.IntersectionObserver) {
     const observer = new IntersectionObserver(
       entries =>
         entries.forEach(entry => {
           if (entry.isIntersecting) {
-            entry.target.classList.add(`cover-${entry.target.dataset.cover}`);
+            addCoverClass(entry.target);
             observer.unobserve(entry.target);
           }
         }),
       { rootMargin: '500% 0%' }
     );
-    [...document.querySelectorAll('.cover')].forEach(cover =>
-      observer.observe(cover)
-    );
+    covers.forEach(cover => observer.observe(cover));
+  } else {
+    covers.forEach(cover => addCoverClass(cover));
   }
 };
