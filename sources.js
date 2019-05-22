16455f949e9e140ab16738eb8494c80d89d03bd2
Danil Semelenov <mail@danil.mobi>
2019-05-23 00:49:45 +0300

    Extract a list of sources to a module

---
+++ sources.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'book-dash': {
+    name: 'Book Dash',
+  },
+  bookbot: {
+    description: '',
+    name: 'Bookbot',
+  },
+  icharacter: {
+    description: '',
+    name: 'iCharacter',
+  },
+  'paul-choy': {
+    description: '',
+    name: 'Paul Choy',
+  },
+  'pratham-books': {
+    description: '',
+    name: 'Pratham Books',
+  },
+  'room-to-read': {
+    description: '',
+    name: 'Room to Read',
+  },
+  rubbishbooks: {
+    description: '',
+    name: 'Rubbish Books',
+  },
+  'storybooks-canada': {
+    description: '',
+    name: 'Storybooks Canada',
+  },
+  storyweaver: {
+    description: '',
+    name: 'StoryWeaver',
+  },
+};
