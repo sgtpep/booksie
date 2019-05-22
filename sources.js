99b49215f370b776e00db69dccf8fa07b15f22bb
Danil Semelenov <mail@danil.mobi>
2019-05-23 00:50:09 +0300

    Add source descriptions

--- sources.js
+++ sources.js
@@ -1,25 +1,32 @@
 module.exports = {
   'book-dash': {
+    description:
+      'Book Dash gathers creative volunteers to create new African storybooks that anyone can freely print, translate and distribute.',
     name: 'Book Dash',
   },
   bookbot: {
-    description: '',
+    description:
+      'Bookbot has a mission to address the worldwide issue of shockingly low literacy levels.',
     name: 'Bookbot',
   },
   icharacter: {
-    description: '',
+    description:
+      'iCharacter creates books, music and videos for children that focus on character building topics.',
     name: 'iCharacter',
   },
   'paul-choy': {
-    description: '',
+    description:
+      'The "Adventures of Little Mouse" books are a charming series of books for children, each featuring stories from the world of Little Mouse and his woodland friends.',
     name: 'Paul Choy',
   },
   'pratham-books': {
-    description: '',
+    description:
+      'Pratham Books is a nonprofit publisher introducing children to the joy of reading.',
     name: 'Pratham Books',
   },
   'room-to-read': {
-    description: '',
+    description:
+      'Room to Read is a non-profit organization for improving literacy and gender equality in education in the developing world.',
     name: 'Room to Read',
   },
   rubbishbooks: {
@@ -27,11 +34,13 @@ module.exports = {
     name: 'Rubbish Books',
   },
   'storybooks-canada': {
-    description: '',
+    description:
+      'Storybooks Canada is a free open educational resource that promotes literacy and language learning in homes, schools, and communities.',
     name: 'Storybooks Canada',
   },
   storyweaver: {
-    description: '',
+    description:
+      "Designed to provide children with reading resources, StoryWeaver is a digital gateway to thousands of richly illustrated, open-licensed children's stories.",
     name: 'StoryWeaver',
   },
 };
