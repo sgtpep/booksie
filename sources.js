825ffd6bf204d510d53e426dd55b1e069eed497f
Danil Semelenov <mail@danil.mobi>
2019-05-24 00:47:42 +0300

    Generate links to book sources programmatically

--- sources.js
+++ sources.js
@@ -2,45 +2,54 @@ module.exports = {
   'book-dash': {
     description:
       'Book Dash gathers creative volunteers to create new African storybooks that anyone can freely print, translate and distribute.',
+    homepage: 'https://bookdash.org/',
     name: 'Book Dash',
   },
   bookbot: {
     description:
       'Bookbot has a mission to address the worldwide issue of shockingly low literacy levels.',
+    homepage: 'https://www.bookbotkids.com/',
     name: 'Bookbot',
   },
   icharacter: {
     description:
       'iCharacter creates books, music and videos for children that focus on character building topics.',
+    homepage: 'https://www.icharacter.org/',
     name: 'iCharacter',
   },
   'paul-choy': {
     description:
       'The "Adventures of Little Mouse" books are a charming series of books for children, each featuring stories from the world of Little Mouse and his woodland friends.',
+    homepage: 'https://www.paulchoy.com/little-mouse',
     name: 'Paul Choy',
   },
   'pratham-books': {
     description:
       'Pratham Books is a nonprofit publisher introducing children to the joy of reading.',
+    homepage: 'https://prathambooks.org/',
     name: 'Pratham Books',
   },
   'room-to-read': {
     description:
       'Room to Read is a non-profit organization for improving literacy and gender equality in education in the developing world.',
+    homepage: 'https://www.roomtoread.org/',
     name: 'Room to Read',
   },
   rubbishbooks: {
     description: '',
+    homepage: '',
     name: 'Rubbish Books',
   },
   'storybooks-canada': {
     description:
       'Storybooks Canada is a free open educational resource that promotes literacy and language learning in homes, schools, and communities.',
+    homepage: 'https://www.storybookscanada.ca/',
     name: 'Storybooks Canada',
   },
   storyweaver: {
     description:
       "Designed to provide children with reading resources, StoryWeaver is a digital gateway to thousands of richly illustrated, open-licensed children's stories.",
+    homepage: 'https://storyweaver.org.in/',
     name: 'StoryWeaver',
   },
 };
