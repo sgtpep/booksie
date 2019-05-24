f6fab0285f2b1007bb73b42f508d1b02f32e10b7
Danil Semelenov <mail@danil.mobi>
2019-05-25 00:40:10 +0300

    Implement lazy loading of <img>

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
 import coverLazyLoading from './cover-lazy-loading.js';
+import lazyLoading from './lazy-loading.js';
 import stickyElements from './sticky-elements.js';
 import viewer from './viewer.js';
 
 coverLazyLoading();
+lazyLoading();
 stickyElements();
 viewer();
