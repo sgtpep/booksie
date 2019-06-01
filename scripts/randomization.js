e2c65edd0131acb4435b91529d5463a3ff273adf
Danil Semelenov <mail@danil.mobi>
2019-06-02 02:03:05 +0300

    Extract functions

--- scripts/randomization.js
+++ scripts/randomization.js
@@ -1,21 +1,32 @@
-export default (onShuffle = () => {}) =>
-  document.querySelector('#randomization').addEventListener('click', event => {
-    if (event.target.tagName === 'A') {
-      if (event.target.dataset.action === 'book') {
-        const books = document.querySelectorAll('.book');
-        books[Math.floor(Math.random() * books.length)].click();
-      } else if (event.target.dataset.action === 'shuffle') {
-        [...document.querySelectorAll('.books')].forEach(books => {
-          const clone = books.cloneNode(true);
-          [...clone.querySelectorAll('.book')].forEach((book, index, books) =>
-            book.parentElement.insertBefore(
-              books[(Math.random() * books.length) | 0],
-              book.parentElement.firstElementChild
-            )
-          );
-          books.parentElement.replaceChild(clone, books);
-        });
-        onShuffle();
-      }
-    }
+const randomBook = () => {
+  const books = document.querySelectorAll('.book');
+  books[Math.floor(Math.random() * books.length)].click();
+};
+
+const shuffleBooks = (onShuffle = () => {}) => {
+  [...document.querySelectorAll('.books')].forEach(books => {
+    const clone = books.cloneNode(true);
+    [...clone.querySelectorAll('.book')].forEach((book, index, books) =>
+      book.parentElement.insertBefore(
+        books[(Math.random() * books.length) | 0],
+        book.parentElement.firstElementChild
+      )
+    );
+    books.parentElement.replaceChild(clone, books);
   });
+  onShuffle();
+};
+
+export default (onShuffle = () => {}) =>
+  document
+    .querySelector('#randomization')
+    .addEventListener(
+      'click',
+      event =>
+        event.target.tagName === 'A' &&
+        (event.target.dataset.action === 'book'
+          ? randomBook()
+          : event.target.dataset.action === 'shuffle'
+          ? shuffleBooks(onShuffle)
+          : undefinded)
+    );
