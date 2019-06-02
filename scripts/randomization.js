1d59e0871ad7cf52d555ef620ac6125f8e8cd867
Danil Semelenov <mail@danil.mobi>
2019-06-02 03:22:50 +0300

    Speed-up book shuffling

--- scripts/randomization.js
+++ scripts/randomization.js
@@ -3,18 +3,32 @@ const randomBook = () => {
   books[Math.floor(Math.random() * books.length)].click();
 };
 
-const shuffleBooks = (onShuffle = () => {}) => {
-  [...document.querySelectorAll('.books')].forEach(books => {
-    const clone = books.cloneNode(true);
-    [...clone.querySelectorAll('.book')].forEach((book, index, books) =>
-      book.parentElement.insertBefore(
-        books[(Math.random() * books.length) | 0],
-        book.parentElement.firstElementChild
-      )
-    );
-    books.parentElement.replaceChild(clone, books);
-  });
-  onShuffle();
+const shuffleBooks = (onShuffle = () => {}) =>
+  [...document.querySelectorAll('.books')]
+    .reduce(
+      (promise, books) =>
+        promise.then(
+          () =>
+            new Promise(resolve =>
+              setTimeout(() => {
+                shuffleSourceBooks(books);
+                resolve();
+              })
+            )
+        ),
+      Promise.resolve()
+    )
+    .then(() => onShuffle());
+
+const shuffleSourceBooks = books => {
+  const clone = books.cloneNode(true);
+  [...clone.querySelectorAll('.book')].forEach((book, index, books) =>
+    book.parentElement.insertBefore(
+      books[(Math.random() * books.length) | 0],
+      book.parentElement.firstElementChild
+    )
+  );
+  books.parentElement.replaceChild(clone, books);
 };
 
 export default (onShuffle = () => {}) =>
