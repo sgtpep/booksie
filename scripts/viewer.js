e5bc830dca914fb5a702f5141283a38fbf6d7404
Danil Semelenov <mail@danil.mobi>
2019-06-04 13:24:32 +0300

    Adjust offsets to fit hardcoded page numbers in

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -420,7 +420,7 @@ const updatePageView = page => {
     if (name === 'lets-read') {
       offsetView(page.view, 50, 50, 50, 50);
     } else if (name === 'mustard-seed-books') {
-      offsetView(page.view, 20, 20, 20, 20);
+      offsetView(page.view, 10, 10, 10, 10);
     } else if (
       ['pratham-books', 'room-to-read', 'storyweaver'].includes(name)
     ) {
