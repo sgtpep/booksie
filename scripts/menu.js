f3f11bd0a41e5c6e400cf6bc344784ea874ed676
Danil Semelenov <mail@danil.mobi>
2019-06-09 18:02:04 +0300

    Implement book downloading

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,26 @@
+const downloadBook = book => {
+  const anchor = document.createElement('a');
+  anchor.download = '';
+  anchor.href = book.dataset.href;
+  anchor.target = '_blank';
+  anchor.click();
+};
+
+const saveForOffline = book => alert('Comming soon!');
+
+const showCopyright = book =>
+  (location = `#${book.dataset.source}/${book.dataset.slug}/copyright`);
+
 export default () =>
   addEventListener('click', event => {
     if (event.target.dataset.action && event.target.closest('.menu')) {
       const book = event.target.closest('.book');
       event.target.dataset.action === 'copyright'
-        ? (location = `#${book.dataset.source}/${book.dataset.slug}/copyright`)
+        ? showCopyright(book)
+        : event.target.dataset.action === 'download'
+        ? downloadBook(book)
         : event.target.dataset.action === 'save'
-        ? alert('Comming soon!')
+        ? saveForOffline(book)
         : undefined;
     }
   });
