b4ea5cf5995b5a0d28997b32b64caa6be6a01c04
Danil Semelenov <mail@danil.mobi>
2019-06-10 22:19:12 +0300

    Review saving to offline logic

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,10 @@ import { coverURL } from './cover-lazy-loading.js';
 
 export const booksKey = 'books';
 
+const coverSize = parseInt(
+  getComputedStyle(document.documentElement).getPropertyValue('--cover-size')
+);
+
 const downloadBook = book => {
   const anchor = document.createElement('a');
   anchor.download = '';
@@ -10,40 +14,74 @@ const downloadBook = book => {
   anchor.click();
 };
 
+const generateCacheURL = (book, extension) =>
+  `books/${book.dataset.source}/${book.dataset.slug}${extension}`;
+
 const saveBook = book =>
   window.caches
     ? caches.open(booksKey).then(cache => {
         const cover = book.querySelector('.cover');
-        return cache
-          .addAll([
-            book.dataset.href,
-            ...[1, 2].map(scale =>
-              coverURL(cover.dataset.cover, scale, cover.dataset.buster)
-            ),
-          ])
-          .then(() => {
+        const left = parseInt(cover.style.backgroundPositionX) / 100;
+        const top = parseInt(cover.style.backgroundPositionY) / 100;
+        return Promise.all([
+          fetch(book.dataset.href).then(response =>
+            cache.put(generateCacheURL(book, '.pdf'), response)
+          ),
+          Promise.all(
+            [1, 2].map(scale =>
+              new Promise((resolve, reject) => {
+                const image = new Image();
+                image.addEventListener('load', () => resolve([image, scale]));
+                image.src = coverURL(
+                  cover.dataset.cover,
+                  scale,
+                  cover.dataset.buster
+                );
+              }).then(([image, scale]) => {
+                const canvas = document.createElement('canvas');
+                canvas.height = coverSize * scale;
+                canvas.width = coverSize * scale;
+                canvas
+                  .getContext('2d')
+                  .drawImage(
+                    image,
+                    left * coverSize * scale,
+                    top * coverSize * scale
+                  );
+                return canvas.toDataURL('image/jpeg', 0.8);
+              })
+            )
+          ).then(images => {
             const clone = book.cloneNode(true);
+            clone.dataset.href = generateCacheURL(book, '.pdf');
+            clone.dataset.offline = true;
             const cover = clone.querySelector('.cover');
             cover.classList.remove(`cover-${cover.dataset.cover}`);
-            const coverClass = `cover-${cover.dataset.cover}-${
-              cover.dataset.buster
-            }`;
-            cover.classList.add(coverClass);
-            const style = [...document.styleSheets[0].rules]
-              .find(style =>
-                style.selectorText.startsWith(`.cover-${cover.dataset.cover},`)
-              )
-              .cssText.replace(/^.+( {)/, `.${coverClass}$1`);
-            return cache.put(
-              `/${book.dataset.source}/${book.dataset.slug}.html`,
-              new Response(
-                clone.outerHTML.replace(/<\/.+>$/, `<style>${style}</style>$&`),
-                {
-                  headers: { 'Content-Type': 'text/html' },
-                }
-              )
+            cover.setAttribute(
+              'style',
+              [...document.styleSheets[0].rules]
+                .find(style =>
+                  style.selectorText.startsWith(
+                    `.cover-${cover.dataset.cover},`
+                  )
+                )
+                .cssText.replace(/^.+{(.+)}$/, '$1')
+                .replace(/(?<=url\()[^)]+/g, url => {
+                  const match = url.match(/@(\d+)x/);
+                  return images[match ? Number(match[1]) - 1 : 0];
+                })
             );
-          });
+            return cache.put(
+              generateCacheURL(book, '.html'),
+              new Response(clone.outerHTML, {
+                headers: {
+                  'Content-Length': clone.outerHTML.length,
+                  'Content-Type': 'text/html',
+                },
+              })
+            );
+          }),
+        ]);
       })
     : alert('Not supported in your browser.');
 
