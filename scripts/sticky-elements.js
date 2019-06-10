07044050a3ea0c836f79c1563b8cbed47f9f13ee
Danil Semelenov <mail@danil.mobi>
2019-06-11 00:21:58 +0300

    Avoid initial sticky header's box-shadow flickering

--- scripts/sticky-elements.js
+++ scripts/sticky-elements.js
@@ -1,12 +1,14 @@
 export default () => {
   if (window.IntersectionObserver) {
     const observer = new IntersectionObserver(entries =>
-      entries.forEach(entry => {
-        entry.target.nextElementSibling.classList.toggle(
-          'sticky',
-          !entry.isIntersecting
-        );
-      })
+      entries.forEach(
+        entry =>
+          entry.boundingClientRect.width &&
+          entry.target.nextElementSibling.classList.toggle(
+            'sticky',
+            !entry.isIntersecting
+          )
+      )
     );
     [...document.querySelectorAll('.stickiness-sentinel')].forEach(sentinel =>
       observer.observe(sentinel)
