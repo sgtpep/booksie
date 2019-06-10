a5e2e200be7facfd888e611ec54522592344099a
Danil Semelenov <mail@danil.mobi>
2019-06-10 16:14:27 +0300

    Implement book saving

--- scripts/cover-lazy-loading.js
+++ scripts/cover-lazy-loading.js
@@ -5,6 +5,9 @@ const addCoverClass = cover =>
 
 const coverPreloaded = {};
 
+export const coverURL = (index, scale = 1, buster = undefined) =>
+  `covers/${index}${scale > 1 ? '@2x' : ''}.jpg${buster ? `?${buster}` : ''}`;
+
 const loadCover = cover => {
   if (!coverPreloaded[cover.dataset.cover]) {
     coverPreloaded[cover.dataset.cover] = true;
@@ -16,9 +19,11 @@ const loadCover = cover => {
         ),
       ].forEach(cover => addCoverClass(cover))
     );
-    image.src = `covers/${cover.dataset.cover}${
-      devicePixelRatio > 1 ? '@2x' : ''
-    }.jpg?${cover.dataset.buster}`;
+    image.src = coverURL(
+      cover.dataset.cover,
+      devicePixelRatio,
+      cover.dataset.buster
+    );
   }
 };
 
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,5 @@
+import { coverURL } from './cover-lazy-loading.js';
+
 const downloadBook = book => {
   const anchor = document.createElement('a');
   anchor.download = '';
@@ -6,7 +8,39 @@ const downloadBook = book => {
   anchor.click();
 };
 
-const saveForOffline = book => alert('Comming soon!');
+const saveBook = book =>
+  window.caches
+    ? caches.open('books').then(cache => {
+        const cover = book.querySelector('.cover');
+        return cache
+          .addAll([
+            book.dataset.href,
+            ...[1, 2].map(scale =>
+              coverURL(cover.dataset.cover, scale, cover.dataset.buster)
+            ),
+          ])
+          .then(() => {
+            const clone = book.cloneNode(true);
+            const cover = clone.querySelector('.cover');
+            cover.classList.remove(`cover-${cover.dataset.cover}`);
+            const coverClass = `cover-${cover.dataset.cover}-${
+              cover.dataset.buster
+            }`;
+            cover.classList.add(coverClass);
+            const style = [...document.styleSheets[0].rules]
+              .find(style =>
+                style.selectorText.startsWith(`.cover-${cover.dataset.cover},`)
+              )
+              .cssText.replace(/^.+( {)/, `.${coverClass}$1`);
+            return cache.put(
+              `/${book.dataset.source}/${book.dataset.slug}.html`,
+              new Response(`${clone.outerHTML}\n${style}`, {
+                headers: { 'Content-Type': 'text/html' },
+              })
+            );
+          });
+      })
+    : alert('Not supported in your browser.');
 
 const showCopyright = book =>
   (location = `#${book.dataset.source}/${book.dataset.slug}/copyright`);
@@ -20,7 +54,7 @@ export default () =>
         : event.target.dataset.action === 'download'
         ? downloadBook(book)
         : event.target.dataset.action === 'save'
-        ? saveForOffline(book)
+        ? saveBook(book)
         : undefined;
     }
   });
