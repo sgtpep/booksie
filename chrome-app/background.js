7f386dc823eea1e09318485411f638ab0d2e9e01
Danil Semelenov <mail@danil.mobi>
2019-07-03 02:04:29 +0300

    Remove commented code

--- chrome-app/background.js
+++ chrome-app/background.js
@@ -1,14 +1,3 @@
 chrome.app.runtime.onLaunched.addListener(() =>
-  chrome.app.window.create(
-    'index.html'
-    /*, createdWindow =>
-    createdWindow.contentWindow.addEventListener('DOMContentLoaded', () =>
-      createdWindow.contentWindow.document
-        .querySelector('webview')
-        .addEventListener('newwindow', event => {
-          event.preventDefault();
-          open(event.targetUrl);
-        })
-    )*/
-  )
+  chrome.app.window.create('index.html')
 );
